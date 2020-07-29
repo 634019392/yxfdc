c6da470bc5b78eedc4730ec42f45d77f2de7464c
ll <634019392@qq.com>
2020-07-29 12:24:42 +0800

    git忽略文件

--- config/alibaba.php
+++ config/alibaba.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'accessKeyId' => '',
-    'accessSecret' => '',
+    'accessKeyId' => 'LTAI4G8WvEDQoipcfVammGGh',
+    'accessSecret' => 'zEIxrM9UzRdsgqnjRYxQaTt0Jc9rn2',
 ];
--- config/qiniu.php
+++ config/qiniu.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'accessKey' => '',
-    'secretKey' => '',
-    'bucket' => '',
-    'http' => '',
+    'accessKey' => '5w_1SysUwnAgl_7N-P2Nvgl-3VZk72XqJtVt_bTk',
+    'secretKey' => '0ufeznfLYzbDQ3zOenh1rKtsHQbiO18tOtgsn1VR',
+    'bucket' => 'laoyangshuofang',
+    'http' => 'http://laoyangshuofang.w20.top/',
 ];
