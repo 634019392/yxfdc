73bd1f8a74e90d2c7838c2407a7ca4933c5e6cfb
ll <634019392@qq.com>
2020-08-14 11:13:05 +0800

    定时任务-超过保护期将状态1和2的更改为5

---
+++ app/Console/Commands/Cron/CustomerFailure.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands\Cron;
+
+use App\Models\Recommender;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class CustomerFailure extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cron:customer-failure';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '推荐客户超过保护期更改为失效状态';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Recommender::query()->where('status', '<=', 2)
+            ->chunkById(1000, function ($item) {
+                foreach($item as $val) {
+                    $now = Carbon::now();
+                    $protect_time = Carbon::parse($val->protect_time);
+                    $diff = $now->diffInDays($protect_time, false);
+                    if ($diff <= 0) {
+                        $val->status = 5;
+                        $val->save();
+                        $this->info('状态更改为5，更新成功的id:' . $val->id);
+                    }
+                }
+            });
+    }
+}
