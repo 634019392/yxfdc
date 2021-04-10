2df94889c8628486d43aaed6c6756b79de533899
ll <634019392@qq.com>
2021-04-10 19:07:52 +0800

    隐藏配置文件gaode

--- config/gaode.php
+++ config/gaode.php
@@ -1,7 +1,7 @@
 <?php
 return [
     // 高德地图的key
-    'key' => '076cdba00c2b332f6d11ca9a1768384c',
+    'key' => env('GAODE_KEY', ''),
     // 把地址转为经纬度  %s 占位符
     'geocode' => 'https://restapi.amap.com/v3/geocode/geo?key=%s&address=%s'
 ];
